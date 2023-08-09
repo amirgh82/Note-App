3ad546da8085f88b59d73840ca5010d255b2e0a9
amirgh82 <amirghhastam@gmail.com>
2023-08-09 14:42:52 +0330

    select elements in DOM

---
+++ app.js
@@ -0,0 +1,10 @@
+const $ = document
+
+const addBox = $.querySelector('.add-box'),
+    popupBox = $.querySelector('.popup-box'),
+    popupTitle = $.querySelector('header p'),
+    popupClose = $.querySelector('header i'),
+    inputElem = $.querySelector('input'),
+    textareaElem = $.querySelector('textarea'),
+    buttonElem = $.querySelector('button')
+
