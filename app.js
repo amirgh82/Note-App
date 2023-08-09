8f0468c222edabeec4483f13dde7623d9e466b49
amirgh82 <amirghhastam@gmail.com>
2023-08-09 15:02:31 +0330

    popup show and close

--- app.js
+++ app.js
@@ -8,19 +8,48 @@ const addBox = $.querySelector('.add-box'),
     textareaElem = $.querySelector('textarea'),
     buttonElem = $.querySelector('button')
 
-    let notes = []
+let isUpdate = false
 
-    function getLocalStorageNotes() {
-        let localStorageNotes = localStorage.getItem('notes')
-    
-        if (localStorageNotes) {
-            notes = JSON.parse(localStorageNotes)
-        } else {
-            notes = []
-        }
-    
-        return notes
-    
+let notes = []
+
+addBox.addEventListener('click', () => {
+
+    if (isUpdate) {
+        popupTitle.innerHTML = 'Update main note '
+        buttonElem.innerHTML = "Update note"
+    } else {
+        popupTitle.innerHTML = 'Add a new note '
+        buttonElem.innerHTML = "Add note"
     }
-    
-    
+
+    popupBox.classList.add('show')
+    inputElem.focus()
+})
+
+popupClose.addEventListener('click', () => {
+    popupBox.classList.remove('show')
+})
+
+function generateNotes(notes) {
+
+}
+
+function getLocalStorageNotes() {
+    let localStorageNotes = localStorage.getItem('notes')
+
+    if (localStorageNotes) {
+        notes = JSON.parse(localStorageNotes)
+    } else {
+        notes = []
+    }
+
+    return notes
+
+}
+
+window.addEventListener('load', () => {
+    let notes = getLocalStorageNotes()
+
+    generateNotes(notes)
+
+})
