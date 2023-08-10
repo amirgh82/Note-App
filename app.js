96f15930dfc70dd03c22adcda27265a56ac0da90
amirgh82 <amirghhastam@gmail.com>
2023-08-10 17:10:42 +0330

    write code delete note

--- app.js
+++ app.js
@@ -1,76 +1,114 @@
 const $ = document
-
 const addBox = $.querySelector('.add-box'),
-    popupBox = $.querySelector('.popup-box'),
+    popupBox = $.querySelector('.popup-box '),
     popupTitle = $.querySelector('header p'),
     popupClose = $.querySelector('header i'),
     inputElem = $.querySelector('input'),
     textareaElem = $.querySelector('textarea'),
     buttonElem = $.querySelector('button'),
-    wrapperElem = $.querySelector('.wrapper'),
-    settings = $.querySelector('.settings')
-
-
-
-
+    wrapperElem = $.querySelector('.wrapper')
 
 let isUpdate = false
 
-
-
-
 let notes = []
 
+addBox.addEventListener('click', () => {
 
+    if (isUpdate) {
+        popupTitle.innerHTML = 'Update main note'
+        buttonElem.innerHTML = 'Update Note'
+    } else {
+        popupTitle.innerHTML = 'Add a new note'
+        buttonElem.innerHTML = 'Add Note'
+    }
+    inputElem.focus()
+    popupBox.classList.add('show')
+})
 
+buttonElem.addEventListener('click', () => {
+    let newNote = {
+        title: inputElem.value,
+        description: textareaElem.value,
+        date: getNowDate()
+    }
+
+    notes.push(newNote)
+    setNotesInLocalStorage(notes)
+    closeModal()
+    generateNotes(notes)
+    clearInputs()
+
+})
+
+function getNowDate() {
+    let now = new Date()
+
+    const months = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"];
+
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let nowDay = now.getDay()
+    let nowMonth = now.getMonth()
+    let nowYear = now.getFullYear()
+    let dayOfMonth = now.getDate()
+
+    // Created_At, Updated_At
+
+    return `${months[nowMonth]} ${dayOfMonth}, ${nowYear} (${days[nowDay]})` // April 12, 2022 (Tuesday)
+}
+
+function clearInputs() {
+    inputElem.value = ''
+    textareaElem.value = ''
+}
 
 function generateNotes(notes) {
 
     $.querySelectorAll('.note').forEach(note => note.remove())
 
-
-    notes.forEach((note) => {
-        wrapperElem.insertAdjacentHTML('beforeend', `<li class="note">
+    notes.forEach((note, index) => {
+        wrapperElem.insertAdjacentHTML('beforeend', `
+        <li class="note">
         <div class="details">
-            <p>${note.title}</p>
-            <span>${note.description}</span>
+          <p>${note.title}</p>
+          <span>${note.description}</span>
         </div>
         <div class="bottom-content">
-            <span>${note.date}</span>
-            <div class="settings">
-                <i class="uil uil-ellipsis-h" onclick="showSetting(this)"></i>
-                <ul class="menu">
-                    <li>
-                        <i class="uil uil-pen"></i>Edit
-                    </li>
-                    <li>
-                        <i class="uil uil-trash"></i>Delete
-                    </li>
-                </ul>
-            </div>
+          <span>${note.date}</span>
+          <div class="settings">
+            <i class="uil uil-ellipsis-h" onclick="showSetting(this)"></i>
+            <ul class="menu">
+              <li>
+                <i class="uil uil-pen"></i>Edit
+              </li>
+              <li onclick="removeNote(${index})">
+                <i class="uil uil-trash"></i>Delete
+              </li>
+            </ul>
+          </div>
         </div>
-    </li>`)
+      </li>
+        `)
     })
 }
 
+function removeNote(noteIndex) {
 
+    let deleted = confirm('Are you sure to delete note?!')
 
+    if (deleted) {
+        let newNotes = getLocalStorageNotes()
 
-
-function setNotesInLocalStoreg(notes) {
-    localStorage.setItem('notes', JSON.stringify(notes))
+        newNotes.splice(noteIndex, 1)
+        setNotesInLocalStorage(newNotes)
+        generateNotes(newNotes)
+    }
 }
 
-
-
-
-
-function showSetting(element) {
-    element.parentElement.classList.add('show')
+function showSetting(el) {
+    el.parentElement.classList.add('show')
 }
-
-
-
 
 function getLocalStorageNotes() {
     let localStorageNotes = localStorage.getItem('notes')
@@ -82,93 +120,27 @@ function getLocalStorageNotes() {
     }
 
     return notes
-
 }
 
+function setNotesInLocalStorage(notes) {
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
 
-
-
-
-addBox.addEventListener('click', () => {
-
-    if (isUpdate) {
-        popupTitle.innerHTML = 'Update main note '
-        buttonElem.innerHTML = "Update note"
-    } else {
-        popupTitle.innerHTML = 'Add a new note '
-        buttonElem.innerHTML = "Add note"
-    }
-
-    popupBox.classList.add('show')
-    inputElem.focus()
-})
-
-
-
-
-
-
-popupClose.addEventListener('click', () => {
+function closeModal() {
     popupBox.classList.remove('show')
-})
+}
 
-
-
-
-
+popupClose.addEventListener('click', closeModal)
 
 window.addEventListener('load', () => {
     let notes = getLocalStorageNotes()
-
     generateNotes(notes)
 })
 
+window.addEventListener('keyup', event => {
 
-
-
-
-window.addEventListener('keyup', (event) => {
-    if (event.keyCode === 27) {
-        popupBox.classList.remove('show')
-    }
-})
-
-
-
-
-
-buttonElem.addEventListener('click', (event) => {
-    event.preventDefault()
-    let newNote = {
-        id: notes.length + 1,
-        title: inputElem.value,
-        description: textareaElem.value,
-        date: getNowDate()
+    if (event.key === 'Escape') {
+        closeModal()
     }
 
-    notes.push(newNote)
-
-    setNotesInLocalStoreg(notes)
-    generateNotes(notes)
-
-    inputElem.value = ''
-    textareaElem.value = ''
-    popupBox.classList.remove('show')
 })
-
-
-function getNowDate() {
-    let now = new Date()
-
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
-    let nowDay = now.getDay()
-    let nowMonth = now.getMonth()
-    let nowYear = now.getFullYear()
-    let dayOfMonth = now.getDate()
-    let hours = now.getHours()
-    let minuts = now.getMinutes()
-
-    return `${months[nowMonth]} ${dayOfMonth} , ${nowYear} (${days[nowDay]}) - ${hours}:${minuts}`
-}
